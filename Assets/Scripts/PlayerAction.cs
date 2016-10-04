c7e78dac0fbd3c7fa0d61e4c4331dd7960dcf6dd
Aleksei Waddington <amw694@nyu.edu>
2016-10-04 16:51:06 -0400

    Started working on getters and setters and stuff for what type of action the player is trying to do

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -2,14 +2,43 @@
 using System.Collections;
 
 public class PlayerAction : MonoBehaviour {
+	public bool isLooking;
+	public bool isTalking;
+	public bool isTouching;
 
-	// Use this for initialization
-	void Start () {
-	
+	public void setLook(bool areTheyCurrentlyLooking){
+		if (areTheyCurrentlyLooking) {
+			isLooking = true;
+		} else {
+			isLooking = false;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void setTalk( bool areTheyCurrentlyTalking){
+		if (areTheyCurrentlyTalking) {
+			isTalking = true;
+		} else {
+			isTalking = false;
+		}
+	}
+
+	public void setTouch( bool areTheyCurrentlyTouching){
+		if (areTheyCurrentlyTouching) {
+			isTouching = true;
+		} else {
+			isTouching = false;
+		}
+	}
+
+	public bool getLook(){
+		return isLooking;
+	}
+
+	public bool getTalk(){
+		return isTalking;
+	}
+
+	public bool getTouch(){
+		return isTalking;
 	}
 }
