a68837368d8dbdee1a5796c99088f759f20d4815
Aleksei Waddington <amw694@nyu.edu>
2016-10-14 13:50:26 -0400

    Added a script that will stop the player's vision collider from colliding with the specified objects (the one the script is attached to);

---
+++ Assets/Scripts/PlayerSpecificScripts/IgnoreCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour {
+	public GameObject visionCone;
+
+	// Use this for initialization
+	void Start () {
+		//The box collider used to check if we are close enough to an object to interact with it
+		//Will no longer also collide with the walls and such, which was limiting the players movement.
+		Physics.IgnoreCollision (visionCone.GetComponent<Collider> (), GetComponent<Collider> ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
